827709eed315b1b05372ef282603e09074490c1f
Conan Chan <chanconan67@gmail.com>
2020-03-16 17:59:57 -0700

    Created simple Scrapy class for testing

--- decks/models.py
+++ decks/models.py
@@ -3,3 +3,20 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+#Simple class for testing Scrapy
+class ScrapyItem(models.Model):
+    unique_id = models.CharField(max_length=100, null=True)
+    data = models.TextField()
+    date = models.DateTimeField()
+
+    @property
+    def to_dict(self):
+        data = {
+            'data': json.loads(self.data),
+            'date': self.date
+        }
+        return data
+
+    def __str__(self):
+        return self.unique_id
