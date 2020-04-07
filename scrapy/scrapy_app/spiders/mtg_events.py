da088814f108faee03838d8d6e441b1c74a12074
Conan Chan <chanconan67@gmail.com>
2020-04-07 16:44:26 -0700

    Updated event spider to list unique events

--- scrapy/scrapy_app/spiders/mtg_events.py
+++ scrapy/scrapy_app/spiders/mtg_events.py
@@ -2,6 +2,7 @@ import scrapy
 
 class MtgEvents(scrapy.Spider):
     name = "mtg_event"
+    event_list = []
 
     def start_requests(self):
         archetype_file = open("deck_archetype.txt", "r")
@@ -12,6 +13,7 @@ class MtgEvents(scrapy.Spider):
         
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
+        self.write_to_file(self.event_list)
     
     def parse(self, response):
         xpath_string = "/html/body/div[3]/div/table"
@@ -19,6 +21,12 @@ class MtgEvents(scrapy.Spider):
         cont = str(item.extract())
         cont = cont.split('event?e=')
         events = [event[:5] for event in cont[1:]] #get the archetype ID 
-        with open('deck_events.txt','w') as f:
+        event_dict = dict.fromkeys(self.event_list)
+        event_dict.update(dict.fromkeys(events))
+        self.event_list = list(event_dict)
+
+    
+    def write_to_file(self, events):
+        with open('deck_events.txt','a') as f:
             contents = '\n'.join(['http://mtgtop8.com/events?e='+e+'&f=ST' for e in events])
             f.write(contents)
